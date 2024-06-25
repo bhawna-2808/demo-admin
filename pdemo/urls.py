c9ffddf8149de81b2610abd0420decec1acec666
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-25 17:30:29 +0530

    ss

--- pdemo/urls.py
+++ pdemo/urls.py
@@ -7,6 +7,9 @@ from .views import *
 
 urlpatterns = [
     path("", index, name='index' ),
+    path("logout/", user_logout, name="logout"),
+
+    path("dashboard", dashboard, name='dashboard'),
     path("button/", button, name='button'),
     path("dropdowns/", dropdowns, name='dropdowns'),
     path("typography/", typography, name='typography'),
@@ -15,7 +18,7 @@ urlpatterns = [
     path("chart/", chart, name='chart'),
     path("table/", table, name='table'),
     path("blank-page/", blank_page, name='blank-page'),
-    path("login/", login, name='login'),
+    # path("login/", login, name='login'),
     path("register/", register, name='register'),
     path("error-404/", error_404, name='error-404'),
     path("error-500/", error_500, name='error-500'),
--- pdemo/views.py
+++ pdemo/views.py
@@ -1,10 +1,51 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from login.models import CustomUser
 # Create your views here.
 
 
 def index(request):
+    try:
+        if request.user.is_authenticated:
+            return redirect("dashboard")
+        if request.method == "POST":
+            email = request.POST["email"]
+            password = request.POST["password"]
+
+            # Fetch user by email
+            try:
+                user = CustomUser.objects.get(email=email)
+                email = user.email
+            except CustomUser.DoesNotExist:
+                messages.error(request, "Invalid email or password!")
+                return redirect("index")
+
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Welcome to the Dashboard!")
+                return redirect("dashboard")
+            else:
+                messages.error(request, "Invalid email or password!")
+                return redirect("index")
+        return render(request, "pages/samples/login.html")
+    except Exception as ex:
+        print("Error in: login_page method", ex)
+        messages.error(request, "An error occurred. Please try again.")
+    return render(request, 'pages/samples/login.html')
+
+
+def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect("index")
+
 
 def button(request):
     return render(request, 'pages/ui-features/buttons.html')
@@ -34,8 +75,8 @@ def blank_page(request):
     return render(request, 'pages/samples/blank-page.html')
 
 
-def login(request):
-    return render(request, 'pages/samples/login.html')
+# def login(request):
+#     return render(request, 'pages/samples/login.html')
 
 
 def register(request):
